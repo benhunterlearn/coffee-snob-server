bf18df67fd789f02a5e9aa7c3d4875894684eaa1
Ben Hunter <learntocode@benhunter.me>
2021-11-01 09:51:26 -0500

    Initial setup.

---
+++ src/main/java/com/swf/coffeesnobserver/CoffeeSnobServerApplication.java
@@ -0,0 +1,13 @@
+package com.swf.coffeesnobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeSnobServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeSnobServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swf/coffeesnobserver/CoffeeSnobServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swf.coffeesnobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeSnobServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
